925ad4cf16fc9f17bd90728eb1a5d96060a48285
niq79 <108571846+niq79@users.noreply.github.com>
2024-09-25 15:17:31 +0300

    More cleaning: create card

--- jb_poc/create_card.py
+++ jb_poc/create_card.py
@@ -2,27 +2,6 @@ import random
 import streamlit as st
 from annotated_text import annotated_text, annotation
 
-def get_position_data():
-
-    type = ["New hire onboarding", "Project onboarding", "Skill development", "Customer onboarding"]
-    team = ["Business", "Product", "Sales", "Tech"]
-    position = ["Product", "Customer Success", "Account", "Business Development", "Development"]
-    level = ["Manager", "Director"]
-    location = ["Helsinki", "Stockholm", "New York", "Berlin", "San Fransisco", "Tokyo"]
-    modules = random.randint(7, 9)
-    length = random.randint(8, 12)
-    position_data = {}
-
-    position_data["type"] = random.choice(type)
-    position_data["team"] = random.choice(team)
-    position_data["position"] = random.choice(position) + " " + random.choice(level)
-    position_data["location"] = random.choice(location)
-    position_data["modules"] = modules
-    position_data["length"] = length
-
-    return position_data
-
-
 def get_card():
 
     type = ["New hire onboarding", "Project onboarding", "Skill development", "Customer onboarding"]
@@ -49,4 +28,4 @@ def get_card():
         "Modules: " + str(position_data["modules"]) + "  \nEst. Length: " + str(position_data["length"]) + " hours" + "  \nLocation: " +
         position_data["location"])
     st.markdown(" ")
-    st.button("View Journey", key=key, type="primary", use_container_width=True, )
+    st.button("View Journey", key=key, type="primary", use_container_width=True)
