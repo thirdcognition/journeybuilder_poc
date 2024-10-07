9839051f5efc77147ea916c69220c2ace18daf27
niq79 <108571846+niq79@users.noreply.github.com>
2024-10-07 13:09:10 +0300

    50 icons for MVP

--- jb_poc/icon_creation.py
+++ jb_poc/icon_creation.py
@@ -1,10 +1,10 @@
-# import openai
+import openai
 import os
 import requests
 import yaml
 
 # Replace 'your-api-key' with your actual OpenAI API key
-# openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 # Load the YAML file containing icon descriptions
 with open('icons/descriptions.yaml', 'r') as file:
@@ -15,28 +15,40 @@ with open('icons/descriptions.yaml', 'r') as file:
 
 # Define the prompt for the image creation
 
+
 number_of_images = 3
 
+negative_prompt = "Make sure that the he image has no additional text or other elements included besides the icon itself."
+
+# Make a request to the OpenAI API to create an image
+# response = openai.Image.create(
+#     model="dall-e-3",
+#
+#     n=1,  # Number of images to generate
+#     size="1024x1024"  # Size of the image
+# )
+
 for image_index, item in descriptions['saas_product_icon'].items():
-    print(f"Generating image for: {item["description"]}")
-    prompt = f"Create an UI icon for a SaaS product, it should be black and white, abstract, and have a hand drawn look and feel with a white background. This is the description for the icon: {item["description"]} and nothing else."
+    print(f"Generating image for: {item['description']}")
+    prompt = f"Create a UI icon image which is used in a B2B SaaS product. The style should be black and white, ink, line drawing with a white background. This is the description of the icon: {item['description']}"
     # Make a request to the OpenAI API to create an image
-    response = openai.Image.create(
-        model="dall-e-3",
-        prompt=prompt,
-        n=number_of_images,  # Number of images to generate
-        size="1024x1024"  # Size of the image
-    )
+    responses = []
+    for i in range(0, number_of_images):
+        print(f'Generate image version {i+1}')
+        response = openai.Image.create(
+           model="dall-e-3",
+           prompt=f"{prompt} {negative_prompt}",
+           n=1,  # Number of images to generate
+           size="1024x1024"  # Size of the image
+        )
+        responses.append(response['data'][0]['url'])
 
-    for i in range(number_of_images):
-        # Extract the image URL from the response
-        image_url = response['data'][i]['url']
-
+    for i in range(0, number_of_images):
         # Download the image
-        image_data = requests.get(image_url).content
+        image_data = requests.get(responses[i]).content
 
-        file_name = f"icons/generated_icons/{image_index}_{i}.png"
+        file_name = f"icons/generated_icons/{image_index}_{i+1}.png"
         # Save the image to a file
         with open(file_name, 'wb') as image_file:
-            image_file.write(image_data)
+           image_file.write(image_data)
         print(f"Image created and saved as: '{file_name}'")
